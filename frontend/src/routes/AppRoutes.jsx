68a5b49a4d81e2502fb744c5315a8dfb95c464bb
Aace <aacejovi@gmail.com>
2025-08-20 15:18:49 +0100

    Fist commit

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,28 @@
+import { AuthLayout } from "../layout/AuthLayout";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+import { SignUp } from "../pages/SignUp";
+import { Login } from "../pages/Login";
+
+export const AppRoutes = () => {
+  const routes = [
+    {
+      path: "auth",
+      element: <AuthLayout />,
+      children: [
+        {
+          path: "register",
+          element: <SignUp />,
+        },
+        {
+          path: "login",
+          element: <Login />,
+        },
+      ],
+    },
+  ];
+
+  const router = createBrowserRouter(routes);
+
+  return <RouterProvider router={router} />;
+};
