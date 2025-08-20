2e6418c8687110dc0a1f6573c5ad771c8bba5ec9
Aace <aacejovi@gmail.com>
2025-08-20 21:24:32 +0100

    That's all for today

--- frontend/src/layout/RootLayout.jsx
+++ frontend/src/layout/RootLayout.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { useAuth } from "../store";
 
 export const RootLayout = () => {
+  const { user } = useAuth();
+
+  console.log(user);
+
   return <div>RootLayout</div>;
 };
--- frontend/src/store/AuthProvider.jsx
+++ frontend/src/store/AuthProvider.jsx
@@ -1,17 +1,33 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
 import handleError from "../utils/handleError";
-import { authenticateUser } from "../api/auth";
+import { authenticateUser, logOut } from "../api/auth";
 import { AuthContext } from ".";
+import { toast } from "sonner";
 
 export default function AuthProvider({ children }) {
   const [accessToken, setAccessToken] = useLocalStorage("BHToken", null);
 
   const [user, setUser] = useState(null);
 
-  useEffect(() => {
+  const handleLogOut = useCallback(async () => {
     try {
-      const getUser = async () => {
+      const res = await logOut();
+      if (res.status === 200) {
+        toast.success(res.data.message, { id: "logout" });
+        setAccessToken(null);
+        setUser(null);
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("There was an error trying to log you out");
+    }
+  }, [setAccessToken]);
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
         if (!accessToken) return;
 
         const res = await authenticateUser(accessToken);
@@ -19,13 +35,13 @@ export default function AuthProvider({ children }) {
         if (res.status === 200) {
           setUser(res.data.user);
         }
-      };
-
-      getUser();
-    } catch (error) {
-      handleError(error);
-    }
-  }, [accessToken]);
+      } catch (error) {
+        handleLogOut();
+        handleError(error);
+      }
+    };
+    getUser();
+  }, [accessToken, handleLogOut]);
 
   return (
     <AuthContext.Provider value={{ accessToken, setAccessToken, user }}>
